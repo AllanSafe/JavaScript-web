50e578828a95254bf9c364a691a25807eb6a789f
AllSafe <alanferreira-@outlook.com>
2025-04-07 09:26:48 -0300

    refactor: improve codebase for cleaner project structure
    Streamlined and optimized for enhanced readability and maintainability

--- script.js
+++ script.js
@@ -1,77 +1,8 @@
-const item = document.getElementById("input-item");
-const botaoSalvarItem = document.getElementById("adicionar-item");
-const listaDeCompras = document.getElementById("lista-de-compras");
-let contador = 0;
+import { adicionarItem } from "./js/adicionarItem.js";
+import { verificarListaComprados } from "./js/verificarListaComprados.js";
 
+const botaoSalvarItem = document.getElementById("adicionar-item");
 botaoSalvarItem.addEventListener("click", adicionarItem);
 
-function adicionarItem(evento) {
-  evento.preventDefault();
-
-  const itemDaLista = document.createElement("li");
-  const containerItemLista = document.createElement("div");
-  containerItemLista.classList.add("item-lista-container");
-
-  const containerNomeDoItem = document.createElement("div");
-
-  const containerCheckbox = document.createElement("div");
-  containerCheckbox.classList.add("container-checkbox");
-
-  const checkboxInput = document.createElement("input");
-  checkboxInput.type = "checkbox";
-  checkboxInput.classList.add("checkbox-input");
-  checkboxInput.id = "checkbox-" + contador++;
-
-  const checkboxLabel = document.createElement("label");
-  checkboxLabel.setAttribute("for", checkboxInput.id);
-
-  checkboxLabel.addEventListener("click", function (evento) {
-    const checkboxInput = evento.currentTarget.querySelector(".checkbox-input");
-    const checkboxCustomizado = evento.currentTarget.querySelector(
-      ".checkbox-customizado"
-    );
-    if (checkboxInput.checked) {
-      checkboxCustomizado.classList.add("checked");
-    } else checkboxCustomizado.classList.remove("checked");
-  });
-
-  const checkboxCustomizado = document.createElement("div");
-  checkboxCustomizado.classList.add("checkbox-customizado");
-
-  checkboxLabel.appendChild(checkboxInput);
-  checkboxLabel.appendChild(checkboxCustomizado);
-
-  containerCheckbox.appendChild(checkboxLabel);
-  containerNomeDoItem.appendChild(containerCheckbox);
-
-  const nomeDoItem = document.createElement("p");
-  nomeDoItem.innerText = item.value;
-  containerNomeDoItem.appendChild(nomeDoItem);
-
-  const containerBotoes = document.createElement("div");
-  const botaoRemover = document.createElement("button");
-  const botaoEditar = document.createElement("button");
-  botaoRemover.classList.add("botao-acao");
-  botaoEditar.classList.add("botao-acao");
-
-  const imagemRemover = document.createElement("img");
-  imagemRemover.src = "img/delete.svg";
-  imagemRemover.alt = "Remover";
-
-  botaoRemover.appendChild(imagemRemover);
-  containerBotoes.appendChild(botaoRemover);
-
-  const imagemEditar = document.createElement("img");
-  imagemEditar.src = "img/edit.svg";
-  imagemEditar.alt = "Editar";
-
-  botaoEditar.appendChild(imagemEditar);
-  containerBotoes.appendChild(botaoEditar);
-
-  containerItemLista.appendChild(containerNomeDoItem);
-  containerItemLista.appendChild(containerBotoes);
-  itemDaLista.appendChild(containerItemLista);
-  listaDeCompras.appendChild(itemDaLista);
-
-  item.value = "";
-}
+const listaComprados = document.getElementById("lista-comprados");
+verificarListaComprados(listaComprados);
